20cbcb85233d69ddfbabfc19aeaecafe7ecb6539
EdgarasAnd <eandrejevas@gmail.com>
2022-10-31 20:55:52 +0200

    feat: :sparkles:

---
+++ src/components/AddUserModal.jsx
@@ -0,0 +1,60 @@
+import { Button, Checkbox, Label, Modal, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+
+export default function AddUserModal() {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <React.Fragment>
+        <Button onClick={() => setShowModal(true)}>+ Add your Info</Button>
+        {showModal ? (
+          <Modal
+            show={true}
+            size="md"
+            popup={true}
+            onClose={() => setShowModal(false)}>
+            <Modal.Header />
+            <Modal.Body>
+              <div className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
+                <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                  Add you information
+                </h3>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="name" value="Your name" />
+                  </div>
+                  <TextInput
+                    id="firstName"
+                    placeholder="First name"
+                    required={true}
+                  />
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="email" value="Your email" />
+                  </div>
+                  <TextInput
+                    id="email"
+                    placeholder="name@company.com"
+                    required={true}
+                  />
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="phone" value="Your phone" />
+                  </div>
+                  <TextInput id="phone" type="phone" required={true} />
+                </div>
+                
+                <div className="w-full">
+                  <Button>Submit</Button>
+                </div>
+                
+              </div>
+            </Modal.Body>
+          </Modal>
+        ) : null}
+      </React.Fragment>
+    </>
+  );
+}
